b44f7ce9a624ceb8743b679046594d83d0336872
James Bullen <jambun@gmail.com>
2020-06-25 16:14:09 +1000

    helpy url when publishing

--- publish.sh
+++ publish.sh
@@ -5,3 +5,6 @@ rsync -avz html/maps hudmol.com:/home/james/public_html/pow
 
 scp $@ hudmol.com:/home/james/public_html/pow/
 
+echo https://james.whaite.com/pow/`basename $@` | tr -d '\n' | pbcopy
+
+echo https://james.whaite.com/pow/`basename $@`
