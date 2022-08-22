858f1f5821ec5a5b1adbf136bd10e0d566ab8373
James Bullen <jambun@gmail.com>
2022-08-22 17:42:15 +1000

    better tile handling for app

--- app/pow.js
+++ app/pow.js
@@ -8,32 +8,37 @@ const opts = {
 };
 
 var tiles = [];
+var origin_tile;
 
 function findTile(lat, lon) {
-    var tile;
+//    var tile;
 
+    // find the tile we're in
     for (const tilemd of metadata) {
-
         var min_lon = Math.min(tilemd.topleft.long, tilemd.topright.long, tilemd.bottomleft.long, tilemd.bottomright.long);
         var max_lon = Math.max(tilemd.topleft.long, tilemd.topright.long, tilemd.bottomleft.long, tilemd.bottomright.long);
         var min_lat = Math.min(tilemd.topleft.lat, tilemd.topright.lat, tilemd.bottomleft.lat, tilemd.bottomright.lat);
         var max_lat = Math.max(tilemd.topleft.lat, tilemd.topright.lat, tilemd.bottomleft.lat, tilemd.bottomright.lat);
 
+        // a bit of a fudge - might be off by one because of slope, but doesn't matter
         if (lat < max_lat && lat > min_lat && lon < max_lon && lon > min_lon) {
-            tile = tilemd;
-            tiles.push(tile);
+            origin_tile = tilemd;
             break;
         }
-
     }
 
-    console.log(tile);
+    // remember the tile we're in and the eight surrounding it
+    for (const tilemd of metadata) {
+        if (Math.abs(tilemd.tilex - origin_tile.tilex) <= 1 && Math.abs(tilemd.tiley - origin_tile.tiley) <= 1) {
+            tiles.push(tilemd);
+        }
+    }
 
-    return tile.filename;
+    return origin_tile.filename;
 };
 
 function coords(lat, lon) {
-    tilemd = tiles[0];
+    tilemd = origin_tile;
 
     var x_t = ((lon - tilemd.topleft.long) / (tilemd.topright.long - tilemd.topleft.long)) * 2000;
     var x_b = ((lon - tilemd.bottomleft.long) / (tilemd.bottomright.long - tilemd.bottomleft.long)) * 2000;
@@ -46,20 +51,27 @@ function coords(lat, lon) {
     var x = Math.round((y_l * x_slope + x_t) / (1 - x_slope * y_slope));
     var y = Math.round((x_t * y_slope + y_l) / (1 - y_slope * x_slope));
 
-    console.log(x, y);
     return [x, y];
 };
 
 function gotPos(pos) {
-    console.log(pos);
     setPos(pos.coords.latitude, pos.coords.longitude);
 };
 
 function setPos(lat, lon) {
     tileFile = findTile(lat, lon);
     var xy = coords(lat, lon);
+    var tmdix = 0;
 
-    document.getElementById('tile').setAttribute('xlink:href', 'https://home.whaite.com/fet/imgraw/NSW_25k_Coast_South/' + tileFile);
+    for (const maptile of document.getElementsByClassName('map-tile')) {
+        tmd = tiles[tmdix];
+
+        maptile.setAttribute('x', (origin_tile.tilex - tmd.tilex) * -2000);
+        maptile.setAttribute('y', (origin_tile.tiley - tmd.tiley) * -2000);
+        maptile.setAttribute('xlink:href', 'https://home.whaite.com/fet/imgraw/NSW_25k_Coast_South/' + tmd.filename);
+
+        tmdix++;
+    }
 
     document.getElementById('target').setAttribute('x', xy[0] - 50);
     document.getElementById('target').setAttribute('y', xy[1] - 50);
